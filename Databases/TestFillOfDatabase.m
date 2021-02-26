287c70203a05d3642d8cec0fcd8a359ce36646f3
Richard Dvořák <48693908+Rievil@users.noreply.github.com>
2021-02-26 09:09:09 +0100

    Small changes to Meas

--- Databases/TestFillOfDatabase.m
+++ Databases/TestFillOfDatabase.m
@@ -7,9 +7,9 @@ execute(conn,'DELETE FROM ProjectList WHERE ID>0');
 names=["CETRIS";"HANKA"];
 desc=["Mìøení cementotøískových desek";...
     "Mìøení lomové energie na alkalicky aktivovaných trámeècích o rozmìrech 40x40x160 mm"];
-startdate=[datetime(2019,1,1,'Format','yyyy-mm-dd hh:mm:ss');datetime(2020,1,1,'Format','yyyy-mm-dd hh:mm:ss')];
-enddate=[datetime(2021,12,31,'Format','yyyy-mm-dd hh:mm:ss');datetime(2021,12,31,'Format','yyyy-mm-dd hh:mm:ss')];
-lastchange=[datetime(now(),'ConvertFrom','datenum','Format','yyyy-mm-dd hh:mm:ss');datetime(now(),'ConvertFrom','datenum','Format','yyyy-mm-dd hh:mm:ss')];
+startdate=[datetime(2019,1,1,'Format','yyyy-MM-dd hh:mm:ss');datetime(2020,1,1,'Format','yyyy-MM-dd hh:mm:ss')];
+enddate=[datetime(2021,12,31,'Format','yyyy-MM-dd hh:mm:ss');datetime(2021,12,31,'Format','yyyy-MM-dd hh:mm:ss')];
+lastchange=[datetime(now(),'ConvertFrom','datenum','Format','yyyy-MM-dd hh:mm:ss');datetime(now(),'ConvertFrom','datenum','Format','yyyy-MM-dd hh:mm:ss')];
 
 datain=table(names,desc,...
         startdate,enddate,lastchange,'VariableNames',...
@@ -17,3 +17,48 @@ datain=table(names,desc,...
     
 
 sqlwrite(conn,'ProjectList',datain)
+%% Create random experiments for each project
+
+selectquery = 'SELECT * FROM ProjectList';
+ProjectList = select(conn,selectquery);
+
+IDs=ProjectList.ID;
+datain=table;
+for i=IDs'
+    
+    for j=1:rand(5)*10
+        data=table(i,sprintf("Experiment %d",j),"Popis",'VariableNames',...
+            {'ProjectID','Name','Description'});
+        datain=[datain; data];
+    end
+end
+
+sqlwrite(conn,'Experiments',datain)
+
+%%
+
+selectquery = 'SELECT * FROM Experiments';
+Experiments = select(conn,selectquery);
+
+%% For each experiment create set of measurements
+
+IDs=Experiments.ID;
+clear datain;
+datain=table;
+
+for i=IDs'
+    
+    for j=1:rand(20)*10
+        data=table(i,datetime(2021,12,31,'Format','yyyy-MM-dd hh:mm:ss'),...
+            int16(rand(1)*100),string([cd '\Master\']),"Popis mìøení v daném dni",'VariableNames',...
+            {'ExpID','Datetime','SpecCount','MasterFolder','Description'});
+        datain=[datain; data];
+    end
+end
+%%
+sqlwrite(conn,'Meas',datain)
+%%
+selectquery = 'SELECT * FROM Meas';
+Meas = select(conn,selectquery);
+%%
+
