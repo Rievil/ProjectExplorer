15e1a37e53b4d2da9fb969a36887a0d458ea6094
Richard Dvořák <48693908+Rievil@users.noreply.github.com>
2021-05-21 12:52:29 +0200

    NewIcons

---
+++ Master/Tools/Variables/ChildInspector.m
@@ -0,0 +1,28 @@
+classdef ChildInspector < Inspector
+    properties
+        Parent;
+        MainParent;
+%         Depth=0;
+    end
+    
+    methods
+        function obj=ChildInspector(InArr,parent)
+            obj@Inspector(InArr);
+            obj.Child=1;            
+            obj.Parent=parent;
+            obj.Node=obj.Parent.ChildNode;
+            obj.MainParent=FindParent(obj);
+            
+            Run(obj);
+        end
+        
+        function parent=FindParent(obj)
+            obj.Depth=1;
+            parent=obj.Parent;
+            while parent.Child==1
+                parent=parent.Parent;
+                obj.Depth=obj.Depth+1;
+            end
+        end
+    end
+end
